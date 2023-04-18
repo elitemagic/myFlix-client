fdec573eee388692f3666cd38c15767a14544af5
elitemagic <toddmerlin@hotmail.com>
2023-04-18 12:13:33 +1000

    g

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -28419,7 +28419,7 @@ const LoginView = ({ onLoggedIn  })=>{
             access: username,
             secret: password
         };
-        fetch("https://openlibrary.org/account/login.json", {
+        fetch("https://my-flix-service.onrender.com/login", {
             method: "POST",
             body: JSON.stringify(data)
         }).then((response)=>{
