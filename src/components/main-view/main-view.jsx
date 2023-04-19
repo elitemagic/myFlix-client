09e2fe1affe41723368a545f8ee721d7e5802140
elitemagic <toddmerlin@hotmail.com>
2023-04-19 15:08:16 +1000

    g

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,78 +1,48 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
+// import { LoginView } from "../login-view/login-view";
 
 
 export const MainView = () => {
 
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
   const [user, setUser] = useState(storedUser? storedUser : null);
-  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-
   useEffect(() => {
-    if (!token) return;
-      
-    fetch("https://my-flix-service.onrender.com/movies", {
-      headers: {Authorization: `Bearer ${token}`}
-    })
-    .then((response) => response.json())
-    .then((movies) => {
-      setMovies(movies);
-      
-    });
-  }, [token]);
+      fetch("https://https://my-flix-service.onrender.com/movies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+          };
+        });
 
- 
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
+        setMovies(moviesFromApi);
+      });
+    }, []);
 
-  if (selectedMovie) {
-    const similarMovies = movies.filter(movie => movie.genre === selectedMovie.genre && movie.title !== selectedMovie.title)
-    return (
-      <>
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-      <hr />
-      <h2>Similar Movies:</h2>
-        
-        {similarMovies.map(movie => (
-          <MovieCard key={movie.id} movie={movie} onMovieClick={setSelectedMovie} />
-        ))}
-        
-        <button style = {{ marginTop: "20px" }}
-        onClick={() => { setUser(null); setToken(null); localStorage.clear();
+    if (selectedMovie) {
+      return (
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      );
+    }
+       
+    if (movies.length === 0) {
+      return (
+        <>
+        <button
+        onClick={() => {
+          setUser(null);
         }}
         >Logout</button>
-      </>
-    );
-  }
-  
-  
-
-  if (movies.length === 0) {
-    return (
-      <>
-      <button
-      onClick={() => {
-        setUser(null);
-      }}
-      >Logout</button>
-      <div>The list is empty</div>;
-      </>
-    );
-  }
+        <div>The list is empty</div>;
+        </>
+      );
+    }
 
   return (
     <div>
@@ -87,8 +57,7 @@ export const MainView = () => {
         />
       ))}
       <button style = {{ marginTop: "20px" }}
-      onClick={() => { setUser(null); setToken(null); localStorage.clear();
-      }}
+      onClick={() => { setUser(null)}}
       >
       Logout
       </button>
